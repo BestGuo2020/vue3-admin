728c43f12cc5e7c84bae56bb6529c0a778dedced
bestguo2020 <bestguo2020@foxmail.com>
2022-10-25 19:52:55 +0800

    设置和用户简要信息

--- src/views/layout/components/Header.vue
+++ src/views/layout/components/Header.vue
@@ -4,15 +4,13 @@
     <Fold v-if="!collapse" @click="handleCollapse" />
   </el-icon>
   <div class="toolbar">
-    <el-dropdown>
-      <el-icon style="margin-right: 8px; margin-top: 1px"><setting /></el-icon>
-      <template #dropdown>
-        <el-dropdown-menu>
-          <el-dropdown-item>退出</el-dropdown-item>
-        </el-dropdown-menu>
-      </template>
-    </el-dropdown>
-    <span>Tom</span>
+    <div class="brief-info">
+      <el-avatar class="brief-avater" :size="24" src="http://avatarurl">
+        <img :src="avatar" />
+      </el-avatar>
+      <span>BestGuo2020</span>
+    </div>
+    <el-icon class="icon-style icon-settings"><setting /></el-icon>
   </div>
 </template>
 
@@ -25,9 +23,13 @@ function handleCollapse() {
   collapse.value = !collapse.value;
   emitter.emit("collapse", collapse.value);
 }
+
+// 默认头像
+const avatar = "https://avatars.githubusercontent.com/u/45250038?v=4";
+
 </script>
 
-<style>
+<style lang="scss">
 .layout-container .el-header {
   position: relative;
   background-color: white;
@@ -44,9 +46,26 @@ function handleCollapse() {
   right: 20px;
 }
 .icon-style {
-  display: inline-flex;
-  align-items: center;
-  justify-content: center;
   height: 100%;
+  cursor: pointer;
+}
+.icon-settings {
+  margin-top: 1px; 
+  font-size: 20px;
+  padding-left: 7px;
+}
+.brief-info {
+  padding: 0 10px;
+  font-size: 14px;
+  display: inline-flex;
+  height: 100%;
+  align-items: center;
+  cursor: pointer;
+}
+.brief-info:hover {
+  background-color: #f6f6f6;
+}
+.brief-avater {
+  margin: 0 8px;
 }
 </style>
