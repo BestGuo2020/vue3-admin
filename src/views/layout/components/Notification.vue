081ec582cba94b0b285b1699471c4cd3d399da22
bestguo2020 <bestguo2020@foxmail.com>
2022-10-26 23:41:20 +0800

    解决宽高出现100%的问题

--- src/views/layout/components/Notification.vue
+++ src/views/layout/components/Notification.vue
@@ -13,70 +13,80 @@
       <el-button type="danger" @click="handleDeleteAll">全部删除</el-button>
     </el-button-group>
     <!-- 消息 -->
-    <el-scrollbar style="margin: 10px 0">
-    <el-empty v-if="notificationData.length === 0" description="还没有消息呢" />
-    <el-card
-      class="box-card"
-      shadow="never"
-      v-for="notification in notificationData"
-      :key="notification.id"
-    >
-      <template #header>
-        <div class="card-header">
-          <div style="line-height: 24px; display: flex">
-            <span>{{ notification.time }}</span>
-            <el-tag class="tag-style" size="small" v-if="!notification.read">未读</el-tag>
-            <el-tag class="tag-style" type="success" size="small" v-else>已读</el-tag>
-          </div>
-          <div>
-            <el-button-group>
-              <el-button
-                type="success"
-                size="small"
-                title="标记为已读"
-                @click="handleReadMessage(notification)"
+    <el-scrollbar style="margin: 10px 0;">
+      <el-empty
+        v-if="notificationData.length === 0"
+        description="还没有消息呢"
+      />
+      <el-card
+        class="box-card"
+        shadow="never"
+        v-for="notification in notificationData"
+        :key="notification.id"
+      >
+        <template #header>
+          <div class="card-header">
+            <div style="line-height: 24px; display: flex">
+              <span>{{ notification.time }}</span>
+              <el-tag class="tag-style" size="small" v-if="!notification.read"
+                >未读</el-tag
               >
-                <i class="fa-regular fa-circle-check"></i>
-              </el-button>
-              <el-button
-                type="danger"
-                size="small"
-                title="删除"
-                @click="handleDeleteMessage(notification)"
+              <el-tag class="tag-style" type="success" size="small" v-else
+                >已读</el-tag
               >
-                <i class="fa-regular fa-trash-can"></i>
-              </el-button>
-            </el-button-group>
+            </div>
+            <div>
+              <el-button-group>
+                <el-button
+                  type="success"
+                  size="small"
+                  title="标记为已读"
+                  @click="handleReadMessage(notification)"
+                >
+                  <i class="fa-regular fa-circle-check"></i>
+                </el-button>
+                <el-button
+                  type="danger"
+                  size="small"
+                  title="删除"
+                  @click="handleDeleteMessage(notification)"
+                >
+                  <i class="fa-regular fa-trash-can"></i>
+                </el-button>
+              </el-button-group>
+            </div>
           </div>
-        </div>
-      </template>
-      <div style="line-height: 1.5em">{{ notification.content }}</div>
-    </el-card>
-  </el-scrollbar>
+        </template>
+        <div style="line-height: 1.5em">{{ notification.content }}</div>
+      </el-card>
+    </el-scrollbar>
   </el-drawer>
 </template>
 
 <script setup>
-import { warningConfirm } from '@/utils/messageBox'
+import { warningConfirm } from "@/utils/messageBox";
 // 消息通知数据
 const notificationData = ref([
   {
-    id: "2",
+    id: "11",
     time: "2022-10-23 15:30:12",
     title: "标题1",
     content:
       "vue3-admin 登录页实现完毕，Vue (读音 /vjuː/，类似于 view) 是一套用于构建用户界面的渐进式框架。与其它大型框架不同的是，Vue 被设计为可以自底向上逐层应用。Vue 的核心库只关注视图层，不仅易于上手，还便于与第三方库或既有项目整合。另一方面，当与现代化的工具链以及各种支持类库结合使用时，Vue 也完全能够为复杂的单页应用提供驱动。",
     read: false,
   },
-  {
-    id: "1",
+]);
+
+for (let i = 10; i > 0; i--) {
+  notificationData.value.push({
+    id: `${i}`,
     time: "2022-10-23",
-    title: "标题2",
+    title: `标题${i}`,
     content: "vue3-admin 项目启动",
     type: "info",
     read: false,
-  },
-]);
+  });
+}
 
 // 弹出层打开
 let drawer = ref(false);
@@ -86,20 +96,24 @@ let title = ref(`消息`);
 let count = ref(0);
 
 // 监听未读消息个数
-watch(notificationData, () => {
-  count.value = notificationData.value.filter(item => !item.read).length;
-  title.value = count.value === 0 ? `消息` : `消息（${count.value}）`
-}, { immediate: true, deep: true });
+watch(
+  notificationData,
+  () => {
+    count.value = notificationData.value.filter((item) => !item.read).length;
+    title.value = count.value === 0 ? `消息` : `消息（${count.value}）`;
+  },
+  { immediate: true, deep: true }
+);
 
 // 改变已读消息的状态
 function handleReadMessage(item) {
-  let refRead = toRef(item, 'read');
+  let refRead = toRef(item, "read");
   refRead.value = true;
 }
 // 改变所有
 function handleReadAll() {
-  notificationData.value = notificationData.value.map(item => {
-    if(!item.read) {
+  notificationData.value = notificationData.value.map((item) => {
+    if (!item.read) {
       item.read = true;
     }
     return item;
@@ -108,19 +122,25 @@ function handleReadAll() {
 
 // 删除消息
 function handleDeleteMessage(item) {
-  warningConfirm('确定删除该条通知？').then(() => {
-    notificationData.value = notificationData.value.filter(elem => elem !== item);
-  }).catch(() => {});
+  warningConfirm("确定删除该条通知？")
+    .then(() => {
+      notificationData.value = notificationData.value.filter(
+        (elem) => elem !== item
+      );
+    })
+    .catch(() => {});
 }
 // 删除所有
 function handleDeleteAll() {
-  warningConfirm('确定全部删除？').then(() => {
-    notificationData.value = [];
-  }).catch(() => {});
+  warningConfirm("确定全部删除？")
+    .then(() => {
+      notificationData.value = [];
+    })
+    .catch(() => {});
 }
 </script>
 
-<style>
+<style lang="scss" scoped>
 .box-card {
   margin-top: 10px;
 }
@@ -140,8 +160,8 @@ function handleDeleteAll() {
   padding-bottom: 20px;
   border-bottom: 1px solid #dddddd;
 }
-.el-drawer__body {
-  /* overflow: hidden; */
+.el-scrollbar {
+  height: calc(100% - 52px);
 }
 .tag-style {
   margin-top: 2px;
