a84a03b7e38ac792a962aa55e070bf217bd39987
besrguo2020 <bestguo2020@foxmail.com>
2022-10-28 15:08:08 +0800

    重新将配置文件生效

--- src/request/index.js
+++ src/request/index.js
@@ -1,20 +1,23 @@
-import axios from 'axios'
+import axios from "axios";
 
 const instance = axios.create({
-  baseURL: 'http://localhost:1023/',
+  baseURL: import.meta.env.VITE_BASE_URL,
   timeout: 1000,
 });
 
 // 响应拦截器处理
-instance.interceptors.response.use(config => {
-  console.log("收到的响应拦截", config);
-  // 正常请求
-  if(config.status === 200) {
-    return config.data;
+instance.interceptors.response.use(
+  (config) => {
+    console.log("收到的响应拦截", config);
+    // 正常请求
+    if (config.status === 200) {
+      return config.data;
+    }
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
   }
-  return config;
-}, error => {
-  return Promise.reject(error);
-})
+);
 
-export default instance
+export default instance;
--- vite.config.js
+++ vite.config.js
@@ -1,42 +1,46 @@
-import { fileURLToPath, URL } from 'node:url'
+import { fileURLToPath, URL } from "node:url";
 
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import vueJsx from '@vitejs/plugin-vue-jsx'
+import { defineConfig, loadEnv } from "vite";
+import vue from "@vitejs/plugin-vue";
+import vueJsx from "@vitejs/plugin-vue-jsx";
 
-import AutoImport from 'unplugin-auto-import/vite'
-import Components from 'unplugin-vue-components/vite'
-import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
+import AutoImport from "unplugin-auto-import/vite";
+import Components from "unplugin-vue-components/vite";
+import { ElementPlusResolver } from "unplugin-vue-components/resolvers";
 
-import path from 'path'
-import { viteMockServe } from 'vite-plugin-mock'
+import path from "path";
+import { viteMockServe } from "vite-plugin-mock";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  server: {
-    port: 1023
-  },
-  plugins: [
-    vue(), 
-    vueJsx(),
-    AutoImport({
-      imports: ['vue', 'vue-router'],
-      resolvers: [ElementPlusResolver()],
-    }),
-    Components({
-      resolvers: [ElementPlusResolver()],
-    }),
-    viteMockServe({
-      mockPath: './src/mock/'
-    })
-  ],
-  resolve: {
-    alias: {
-      // 设置路径 这里resolve和join可自行选用
-      '~': path.resolve(__dirname, './'),
-      // 设置别名
-      '@': path.resolve(__dirname, './src')
+export default ({ mode }) => {
+  const env = loadEnv(mode, process.cwd());
+
+  return defineConfig({
+    server: {
+      port: env.VITE_PORT,
     },
-    extensions: ['.mjs', '.js', '.ts', '.jsx', '.tsx', '.json', '.vue']
-  }
-})
+    plugins: [
+      vue(),
+      vueJsx(),
+      AutoImport({
+        imports: ["vue", "vue-router"],
+        resolvers: [ElementPlusResolver()],
+      }),
+      Components({
+        resolvers: [ElementPlusResolver()],
+      }),
+      viteMockServe({
+        mockPath: "./src/mock/",
+      }),
+    ],
+    resolve: {
+      alias: {
+        // 设置路径 这里resolve和join可自行选用
+        "~": path.resolve(__dirname, "./"),
+        // 设置别名
+        "@": path.resolve(__dirname, "./src"),
+      },
+      extensions: [".mjs", ".js", ".jsx", ".json", ".vue"],
+    },
+  });
+};
