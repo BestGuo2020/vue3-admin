b0df98056680819ca1b8be4aff17db6c2354493b
besrguo2020 <bestguo2020@foxmail.com>
2022-10-28 23:47:25 +0800

    新增：状态管理 pinia 以及其它内容

--- src/main.js
+++ src/main.js
@@ -3,11 +3,14 @@ import App from './App.vue'
 
 import router from './router'
 import Particles from 'particles.vue3'
+import { createPinia } from 'pinia'
 
 import 'animate.css'
 
+const pinia = createPinia()
 const app = createApp(App)
 
 app.use(router)
+app.use(pinia)
 app.use(Particles)
 app.mount('#app')
---
+++ src/views/login/hooks/useLogin.js
@@ -0,0 +1,67 @@
+import { post } from '@/request/utils'
+import { useRouter } from 'vue-router'
+import { useMainStore } from '@/store'
+
+export default function () {
+  // 路由引入
+  const router = useRouter()
+  const mainStore = useMainStore()
+
+  // 登录表单中的数据
+  const loginForm = reactive({
+    username: 'admin',
+    password: '111111',
+  })
+
+  // 登录按钮状态
+  const loading = ref(false)
+
+  const ruleFormRef = ref() // 定义一个 ref 用于直接获取表单的信息
+
+  // 定义校验的规则
+  const formRules = {
+    username: [{ required: true, message: '请输入用户名', trigger: 'blur' }],
+    password: [
+      { required: true, message: '请输入密码', trigger: 'blur' },
+      { min: 6, max: 18, message: '密码长度必须在6-18之间', trigger: 'blur' },
+    ],
+  }
+
+  // 登录操作
+  async function login(formEl) {
+    if (!formEl) {
+      return
+    }
+    const { username, password } = loginForm
+    // 表单验证
+    await formEl.validate((valid, fields) => {
+      if (valid) {
+        // 登录
+        loading.value = true
+        post('/api/login', { username, password }).then((res) => {
+          if (res.code === 0) {
+            ElMessage({
+              type: 'success',
+              message: res.msg,
+              onClose: () => {
+                loading.value = false
+                // 保存用户信息并获取 token，
+                mainStore.userInfo = res.data
+                mainStore.token = res.data.token
+                // 跳转
+                router.push('/')
+              },
+            })
+          } else {
+            ElMessage.error(res.msg)
+            loading.value = false
+          }
+        })
+      } else {
+        console.log('error submit!', fields)
+      }
+    })
+  }
+
+  return { loginForm, login, ruleFormRef, formRules }
+}
