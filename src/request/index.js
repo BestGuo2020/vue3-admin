08dc257b7885c2410c211f564ba1221ac51895dd
besrguo2020 <bestguo2020@foxmail.com>
2022-11-01 18:01:08 +0800

    清除打印

--- src/request/index.js
+++ src/request/index.js
@@ -8,7 +8,6 @@ const instance = axios.create({
 
 // 请求前的拦截处理
 instance.interceptors.request.use(config => {
-  console.log(config)
   // 协带 token
   config.headers.token = localStorage.getItem('token')
   return config
@@ -17,7 +16,6 @@ instance.interceptors.request.use(config => {
 // 响应拦截器处理
 instance.interceptors.response.use(
   config => {
-    console.log('收到的响应拦截', config)
     // 正常请求
     if (config.status === 200) {
       // 判断登录状态是否过期
