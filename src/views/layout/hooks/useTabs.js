5c215fe7bd9f9d5a13a1cd3acf601e5c7e322d31
besrguo2020 <bestguo2020@foxmail.com>
2022-11-04 10:49:38 +0800

    功能封装

--- src/views/layout/hooks/useTabs.js
+++ src/views/layout/hooks/useTabs.js
@@ -80,3 +80,45 @@ export function tabsOperation() {
 
   return { openedMenu, tabClose, tabClick }
 }
+
+export function tabTransform(tablist, tablistwrap) {
+  const showSide = ref(true)
+  const offset = ref(0)
+
+  // 左按钮（其实是向右移动，右按钮反之）
+  function translateLeftHandle() {
+    const everyTimeOffset = 200
+
+    // 如果偏移量为0，就不再点左端了
+    if (offset.value < 0) {
+      // 判断左偏移时大于0的情况
+      if (offset.value + everyTimeOffset >= 0) {
+        offset.value += Math.abs(offset.value)
+      } else {
+        offset.value += everyTimeOffset
+      }
+    }
+  }
+
+  function translateRightHandle() {
+    const everyTimeOffset = 200
+
+    // 计算出 tab-list 和 tab-list-wrapper 的差值，也就是右侧不可见的长度部分
+    // 差值≤0：不移动，差值>0，移动
+    let deltaWidth = tablist.value.offsetWidth - tablistwrap.value.offsetWidth
+
+    // tab-list 中的宽度是由 tab 撑大的，如果超过了父元素的宽度就可以移动了
+    if (deltaWidth > 0) {
+      let remainWidth = deltaWidth - Math.abs(offset.value)
+
+      // 剩余右侧不可见长度在100以上，继续用100减，在 100 以内则用remainWidth减去
+      if (remainWidth >= everyTimeOffset) {
+        offset.value -= everyTimeOffset
+      } else if (remainWidth > 0 && remainWidth < everyTimeOffset) {
+        offset.value -= remainWidth
+      }
+    }
+  }
+
+  return { showSide, offset, translateLeftHandle, translateRightHandle }
+}
