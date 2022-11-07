1cb2da250f065162c7f9ee206100ba5034116cf9
bestguo2020 <bestguo2020@foxmail.com>
2022-11-07 22:44:25 +0800

    修复默认主题不生效，以及修改 dom 的获取方式

--- src/components/Echarts/Index.vue
+++ src/components/Echarts/Index.vue
@@ -1,11 +1,13 @@
 <template>
-  <div id="echart" :style="customStyle"></div>
+  <div ref="chartdom" :style="customStyle"></div>
 </template>
 
 <script setup>
 import * as echarts from 'echarts'
-import { onMounted } from 'vue'
+import { onMounted, onUnmounted } from 'vue'
 import { theme } from './theme'
+
+const chartdom = ref()
 
 const props = defineProps({
   customStyle: {
@@ -21,14 +23,20 @@ const props = defineProps({
 
 // 注册主题
 echarts.registerTheme('vintage', theme)
+const resizeChart = () => {
+  myChart.resize()
+}
 
 let myChart = null
 onMounted(() => {
   // 基于准备好的dom，初始化echarts实例
-  myChart = echarts.init(document.getElementById('echart', 'vintage'))
-  window.addEventListener('resize', () => {
-    myChart.resize()
-  })
+  myChart = echarts.init(chartdom.value, 'vintage')
+  window.addEventListener('resize', resizeChart)
+})
+
+onUnmounted(() => {
+  // 卸载大小重置事件
+  window.removeEventListener('resize', resizeChart)
 })
 
 function setOption(option) {
