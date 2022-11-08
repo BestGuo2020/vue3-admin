7ed45f81632036f844eea5e437c6044c22b58635
bestguo2020 <bestguo2020@foxmail.com>
2022-11-08 23:32:08 +0800

    设置默认角色为第一项

--- src/store/index.js
+++ src/store/index.js
@@ -4,16 +4,16 @@ import { get } from '../request/utils'
 export const useMainStore = defineStore('main', () => {
   let userInfo = ref({}) // 用户信息
   let routeLoaded = ref(false) // 路由是否加载完成
-  let role = ref({})
+  let role = ref(localStorage.getItem('role'))
 
   function getUserInfo() {
     get('/api/userinfo', {}).then(res => {
       console.log(res)
       if (res.code === 0) {
         userInfo.value = res.data
-        role.value = res.data.roles[0]
+        role.value = res.data.roles[0].sign
         // 保存角色信息
-        localStorage.setItem('role', res.data.roles[0])
+        localStorage.setItem('role', res.data.roles[0].sign)
       }
     })
   }
