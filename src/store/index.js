159105a15ba90cf5ad82a654ab109c9d35033f18
bestguo2020 <bestguo2020@foxmail.com>
2022-11-08 20:05:26 +0800

    角色保存至到本地存储

--- src/store/index.js
+++ src/store/index.js
@@ -4,15 +4,19 @@ import { get } from '../request/utils'
 export const useMainStore = defineStore('main', () => {
   let userInfo = ref({}) // 用户信息
   let routeLoaded = ref(false) // 路由是否加载完成
+  let role = ref({})
 
   function getUserInfo() {
     get('/api/userinfo', {}).then(res => {
       console.log(res)
       if (res.code === 0) {
         userInfo.value = res.data
+        role.value = res.data.roles[0]
+        // 保存角色信息
+        localStorage.setItem('role', res.data.roles[0])
       }
     })
   }
 
-  return { userInfo, routeLoaded, getUserInfo }
+  return { userInfo, routeLoaded, getUserInfo, role }
 })
